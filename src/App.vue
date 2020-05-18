6b2f62c908b2ccd2aeea07be7bf61df2f993c5c3
JDGiraldo <giraldo.juan.die@gmail.com>
2020-05-17 22:36:23 -0500

    tu huella

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="d-flex">
+        <div class="col-12">
+          <ComponenteHeader></ComponenteHeader>
+        </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
+import ComponenteHeader from './components/ComponenteHeader.vue';
+import ComponenteHuella from './components/ComponenteHuella.vue';
 
 @Component({
   components: {
     HelloWorld,
+    ComponenteHeader,
+    ComponenteHuella,
   },
 })
 export default class App extends Vue {}
@@ -19,11 +26,18 @@ export default class App extends Vue {}
 
 <style>
 #app {
+   background-image: url('./assets/boy.jpg');
+   overflow-y: hidden;
+   height: 100vh;
+   overflow-x: hidden;
+   margin: auto;
+   display:block;
+   background-size: cover;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/ComponenteHuella.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row col-12 mx-auto">
+        <h1>TU HUELLA</h1>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ComponenteTitulo extends Vue {
+
+}
+</script>
+<style>
+
+</style>
